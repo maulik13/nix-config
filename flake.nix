9def7e6b96e185199062c95f1f86c24b0f6138c8
maulik13 <maulik.kataria@gmail.com>
2024-10-15 15:26:00 +0200

    first commit

---
+++ flake.nix
@@ -0,0 +1,96 @@
+{
+  description = "Diaballin simple system configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    # nix-homebrew allows you to configure homebrew declaratively, so the taps
+    # can be managed by nix as well. Keeps all versions predictable!
+    nix-homebrew = {
+      url = "github:zhaofengli/nix-homebrew";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
+    homebrew-bundle = {
+      url = "github:homebrew/homebrew-bundle";
+      flake = false;
+    };
+  };
+
+  outputs = inputs@{ 
+      self,
+      nixpkgs,
+      nix-darwin,
+      nix-homebrew,
+      homebrew-core,
+      homebrew-cask,
+      homebrew-bundle }:
+  let
+    configuration = { lib, pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment = {
+        systemPackages =
+        [
+          pkgs.vim
+          pkgs.cargo
+          pkgs.tmux
+          # pkgs.taskwarrior3
+          (pkgs.writeShellScriptBin "taskw" "exec -a $0 ${pkgs.taskwarrior3}/bin/task $@")
+        ];
+      };
+
+      # Auto upgrade nix package and the daemon service.
+      services.nix-daemon.enable = true;
+      # nix.package = pkgs.nix;
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Create /etc/zshrc that loads the nix-darwin environment.
+      programs.zsh.enable = true;  # default shell on catalina
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 5;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+
+      security.pam.enableSudoTouchIdAuth = true;
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#mk-stacc-mac
+    darwinConfigurations."mk-stacc-mac" = nix-darwin.lib.darwinSystem {
+      modules = [
+        configuration
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            enable = true;
+            # Enable for Apple silicon
+            enableRosetta = true;
+            user = "maulik";
+            autoMigrate = true;
+          };
+        }
+      ];
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."mk-stacc-mac".pkgs;
+  };
+}
